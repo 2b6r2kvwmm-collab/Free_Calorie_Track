08c0a864adef6a5f2e42b3d3d064e532f4beca46
2b6r2kvwmm-collab <2b6r2kvwmm@privaterelay.appleid.com>
2026-01-14 12:41:43 -0500

    Update version.js to 1.5.0 for service worker cache busting

--- calorie-tracker/src/version.js
+++ calorie-tracker/src/version.js
@@ -1,3 +1,3 @@
 // App version - update this whenever you deploy changes
-export const APP_VERSION = '1.4.0';
-export const VERSION_DATE = '2026-01-11';
+export const APP_VERSION = '1.5.0';
+export const VERSION_DATE = '2026-01-14';
